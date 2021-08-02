8d3dbd5660ac7967cab65150b10f35be0544e783
gideon <gideon@hubtel.com>
2021-08-02 10:25:58 +0000

    updated imports

--- muipaginate.js
+++ muipaginate.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { ButtonGroup } from "@material-ui/core";
-import { Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+// import { ButtonGroup } from "@material-ui/core";
+// import { Button } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
